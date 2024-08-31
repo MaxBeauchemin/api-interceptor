e6e9c451d43fcb335eb4c7944b2c537cafd31fc2
mbeauchemin <mbeauchemin@arrivelogistics.com>
2024-08-31 08:38:26 -0500

    Add Sample API app

---
+++ ApiInterceptor/ApiInterceptor.SampleApi/Program.cs
@@ -0,0 +1,72 @@
+using System.Net;
+using ApiInterceptor.DTOs;
+using ApiInterceptor.Filters;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region API Interceptor Setup
+
+var apiInterceptorOptions = new Options
+{
+    Enabled = true,
+    Scenarios = new List<Scenario>
+    {
+        new ()
+        {
+            Name = "SampleScenario",
+            Enabled = true,
+            Filter = new ()
+            {
+                Identities = new List<string> { "TestIdentity" },
+                Endpoints = new List<FilterEndpoint>
+                {
+                    new ()
+                    {
+                        MethodType = "GET",
+                        URL = "*"
+                    }
+                }
+            },
+            Actions = new ()
+            {
+                RespondWith = new ()
+                {
+                    HttpCode = HttpStatusCode.Gone,
+                    Body = new {
+                        TestBody = true
+                    }
+                }
+            }
+        }
+    }
+};
+
+var identityProvider = (ActionExecutingContext ctx) =>
+{
+    var identityHeader = ctx.HttpContext.Request.Headers.FirstOrDefault(h => h.Key == "X-Identity");
+
+    return identityHeader.Value.ToString();
+};
+
+var filter = new ApiInterceptorFilterAttribute(apiInterceptorOptions, identityProvider);
+
+filter.Order = int.MinValue;
+    
+builder.Services.AddMvc(opts =>
+{
+    opts.Filters.Add(filter);
+});
+
+#endregion
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
+
+//To test this code, you can run the app and call the following command from your terminal window
+//curl http://localhost:5153/api/v1/Sample/Test
+//then you can call this to see it getting intercepted
+//curl http://localhost:5153/api/v1/Sample/Test --header "X-Identity: TestIdentity"
